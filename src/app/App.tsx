b4ba092ba04d88808c7dac2092a55ef4f8b4d27c
Andrey <3ntercap@gmail.com>
2022-10-03 16:41:00 +0300

    add babel and i18n extract

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,24 @@
-import { Suspense } from 'react';
-import { Link } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
-
 import './styles/index.scss';
 
-import { classNames } from 'helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
-
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-
-
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 const App = () => {
 
-	const { theme, toggleTheme } = useTheme();
+	const { theme } = useTheme();
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
-			<button onClick={toggleTheme}>TOGGLE THEME</button>
-			<Link to={'/'}>Главная</Link>
-			<Link to={'/about'}>О сайте</Link>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-					<Route path={'/about'} element={<AboutPage />} />
-					<Route path={'/'} element={<MainPage />} />
-				</Routes>
+			<Suspense fallback=''>
+				<Navbar />
+				<div className='content-page'>
+					<Sidebar />
+					<AppRouter />
+				</div>
 			</Suspense>
 		</div>
 	);
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,19 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import AppLink from 'shared/ui/AppLink/AppLink';
+
+import cls from './Navbar.module.scss'
+
+interface NavbarProps {
+	className?: string;
+}
+
+const Navbar = ({ className }: NavbarProps) => {
+	return (
+		<div className={classNames(cls.Navbar, {}, [className])}>
+			<AppLink to={'/'}>Главная</AppLink>
+			<AppLink to={'/about'}>О сайте</AppLink>
+		</div>
+	);
+};
+
+export default Navbar;
